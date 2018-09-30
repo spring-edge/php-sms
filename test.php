7fd0764b585c46119463d52176b438a633f3ec57
Balwant.singh <balwant.singh@impelsys.com>
2018-10-01 00:37:01 +0530

    response print

--- test.php
+++ test.php
@@ -5,6 +5,7 @@ $apikey = "675031xxxxxxxxxxxx";
 $sender = "SEDEMO";
     
 $sendsms = new smsapi($apikey, $sender);
-$sendsms->send_sms("9199xxxxxxxx", "Hi this is a test message");
+$response = $sendsms->send_sms("9199xxxxxxxx", "Hi this is a test message");
 
+echo $response;
 ?> 
